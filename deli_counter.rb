389fee8eb3037544629c955da107add81727d210
Stephen Blais <stephen@s-blais.com>
2019-08-20 18:17:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,6 @@ end
 def take_a_number (katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-
 end
 
 def now_serving (katz_deli)
@@ -25,5 +24,4 @@ def now_serving (katz_deli)
     puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift
   end
-
 end
