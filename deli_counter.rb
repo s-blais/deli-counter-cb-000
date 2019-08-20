d6ed06f1ac6355c5e6f05163f7097da8b2c8f63d
Stephen Blais <stephen@s-blais.com>
2019-08-20 18:05:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 def line (katz_deli)
   if katz_deli.length == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     line_output = "The line is currently:"
     line_position = 1
     katz_deli.each do |customer|
-      line_output.postpend " #{line_position}. customer"
+      line_output.append " #{line_position}. customer"
       line_position += 1
     end
     line_output
